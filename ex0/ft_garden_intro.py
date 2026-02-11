1c699ab653d5dae730d16fd89516972f14598b7d
khaled <khaledhmmddd123@gmail.com>
2026-02-11 16:42:41 +0200

    God its cold

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    name = "Poesie"
+    height = 70
+    age = 27
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}")
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days\n")
+    print("=== End of Program ===")
